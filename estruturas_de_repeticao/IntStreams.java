05ae9b9128d60fb3851d659cda008f3e3759ffa8
Andrei Bissolotti <andreibisso@gmail.com>
2021-09-09 17:28:40 -0300

    IntStream

---
+++ estruturas_de_repeticao/IntStreams.java
@@ -0,0 +1,16 @@
+package estruturas_de_repeticao;
+
+import java.util.stream.IntStream;
+
+public class IntStreams {
+    public static void main(String[] args) {
+        
+        IntStream.of(1,2,3,4,5).forEach(n -> {
+            System.out.println(n);
+        });
+
+        IntStream.range(0, 3).forEach(n -> {
+            System.out.println("Número = "+n);
+        });
+    }
+}
